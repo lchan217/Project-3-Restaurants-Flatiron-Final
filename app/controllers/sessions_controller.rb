cfd64bb985d7ac11730d0fea0576325016b70677
Lizzie <lchan217@gmail.com>
2019-05-13 14:28:42 -0500

    change error messages for sessions and users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,9 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:user][:password])
        session[:user_id] = @user.id
        redirect_to user_path(@user)
+     elsif @user == nil
+       flash[:notice] = "User not found. Please try again. "
+       redirect_to login_path
      else
        flash[:notice] = "There was a problem authenticating your password. Please try again. "
        redirect_to login_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
+      flash[:notice] = @user.errors.full_messages
       render :new
     end
   end
