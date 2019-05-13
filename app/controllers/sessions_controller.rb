92f4d270659905c10fec41f2bb7f5d45a980e2c4
Lizzie <lchan217@gmail.com>
2019-05-13 09:32:40 -0500

    add sessions and users controller, views, and routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationRecord
+  def new
+  end
+  
+  def create
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationRecord
+  def new
+    @user = User.new 
+  end
+  def create
+  end
+end
