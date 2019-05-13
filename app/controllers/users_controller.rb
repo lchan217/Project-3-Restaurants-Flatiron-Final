6eac5aac1ee699ae1fe4dad0cd0c851b08b5d7e7
Lizzie <lchan217@gmail.com>
2019-05-13 09:57:47 -0500

    add bcrypt gem, new and create in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
+    @user = User.new(user_params)
+    if @user.save && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to '/home'
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :username, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  get '/home' => 'users#home'
 end
