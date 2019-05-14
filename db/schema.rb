2803a1c76366e93edda5573a6af90b03b4a459e3
Lizzie <lchan217@gmail.com>
2019-05-14 13:41:22 -0500

    add items  model, table, and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_183449) do
+ActiveRecord::Schema.define(version: 2019_05_14_183847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.boolean "vegetarian?"
+    t.string "calories"
+    t.boolean "lunch_special?"
+    t.string "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "city"
