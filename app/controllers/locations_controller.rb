38486b1c2771bbbc1e99f60c8287cc76e257657d
Lizzie <lchan217@gmail.com>
2019-05-14 13:51:32 -0500

    add index, new, create to locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,22 @@
 class LocationsController < ApplicationController
+  def index
+    @location = Location.all
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to locations_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def location_params
+  end
 end
