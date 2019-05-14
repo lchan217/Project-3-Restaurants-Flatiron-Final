bf106ce26244d44f40cef5769c51dd3236ae2b5b
Lizzie <lchan217@gmail.com>
2019-05-14 15:45:31 -0500

    update locations show page

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,8 @@
 class Location < ActiveRecord::Base
   belongs_to :user
-  belongs_to :restaurant 
+  belongs_to :restaurant
+
+  validates :state, presence: true
 
   def city_and_state
     self.city + ", " + self.state
