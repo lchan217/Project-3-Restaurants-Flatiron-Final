c4dc2ee479368aeb303c653e6f3c66f03709ec2e
Lizzie <lchan217@gmail.com>
2019-05-14 16:18:33 -0500

    add helper method to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,10 @@
 class Restaurant < ApplicationRecord
-  has_many :locations 
+  has_many :locations
   has_many :items
+
+  def location
+    if location_id
+      Location.find(self.location_id)
+    end
+  end
 end
