96187d79320b6d434c01f1a9370ff3d4730662c9
Lizzie <lchan217@gmail.com>
2019-05-15 13:06:46 -0500

    add yes/no values to booleans in items and restaurants

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :restaurant
 
+  def vegetarian
+    self.vegetarian? ? "Yes" : "No"
+  end
+
+  def lunch_special
+    self.lunch_special? ? "Yes" : "No"
+  end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,19 @@
 class Restaurant < ApplicationRecord
   has_many :locations
   has_many :items
-  accepts_nested_attributes_for :locations 
+  accepts_nested_attributes_for :locations
 
   def location
     if self.location_id
       Location.find(self.location_id)
     end
+  end
+
+  def wifi?
+    self.wifi ? "Yes" : "No"
+  end
+
+  def reservations
+    self.reservations? ? "Yes" : "No"
   end
 end
