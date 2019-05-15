e139067e207bc27ec6db1ae9333de68d49d8c930
Lizzie <lchan217@gmail.com>
2019-05-15 17:42:04 -0500

    add location_attributes to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,19 @@
 class Restaurant < ApplicationRecord
   has_many :locations
   has_many :items
-  accepts_nested_attributes_for :locations
+  has_many :users, through: :locations
+
+  #accepts_nested_attributes_for :locations
+
+  def locations_attributes=(locations_attributes)
+   locations_attributes.values.each do |location_atts|
+     if location_atts[:city] != "" && location_atts[:state] != ""
+       # location= location.create(location_atts)
+       # self.locations << location
+       self.locations.build(location_atts)
+     end
+   end
+ end
 
   def location
     if self.location_id
