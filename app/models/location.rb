61e75bbfdea77ac5bb8f963f2845231b1868f094
Lizzie <lchan217@gmail.com>
2019-05-15 17:42:51 -0500

    add hidden field to new form

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,13 +1,11 @@
 class Location < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :restaurant
-
-  validates :state, presence: true
+  belongs_to :user, optional: true
+  belongs_to :restaurant, optional: true
 
   def city_and_state
     self.city + ", " + self.state
   end
-  # 
+  #
   # def location
   #   Restaurant.find(self.restaurant_id)
   # end
