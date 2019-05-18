6177cf667e7a3ca4876b1282c4527e025edb87d0
Lizzie <lchan217@gmail.com>
2019-05-18 11:10:39 -0500

    redirect exisitng user to restaurants index if they try to make a new account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if @user = current_user
+      redirect_to restaurants_path
+    else
+      @user = User.new
+    end
   end
+
   def create
     @user = User.new(user_params)
     if @user.save && @user.authenticate(params[:user][:password])
