48513f880cd1aedbf45ca94e7322fb1151cc2afe
Lizzie <lchan217@gmail.com>
2019-05-19 16:07:37 -0500

    add state to filter page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,10 +3,13 @@ class LocationsController < ApplicationController
   def all_restaurants_in_db
     if params[:query_search] && params[:query_existing] == ""
       @location = Location.search(params[:query_search])
+      @state = params[:query_search]
     elsif params[:query_existing] && params[:query_search] == ""
       @location = Location.search(params[:query_existing])
+      @state = params[:query_existing]
     else
       @location = Location.all
+      @state = "All results"
     end
     render 'all_restaurants_in_db'
   end
