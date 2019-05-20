ea3694d2e675ddcf17b37e1e1fc6a1e99dad5439
Lizzie <lchan217@gmail.com>
2019-05-19 21:30:55 -0500

    sort all restaurants by city

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,13 +2,13 @@ class LocationsController < ApplicationController
     before_action :require_login
   def all_restaurants_in_db
     if params[:query_search] && params[:query_existing] == ""
-      @location = Location.search(params[:query_search])
+      @location = Location.search(params[:query_search]).order(:city)
       @state = params[:query_search]
     elsif params[:query_existing] && params[:query_search] == ""
-      @location = Location.search(params[:query_existing])
+      @location = Location.search(params[:query_existing]).order(:city)
       @state = params[:query_existing]
     else
-      @location = Location.all
+      @location = Location.all.order(:city)
       @state = "All results"
     end
     render 'all_restaurants_in_db'
