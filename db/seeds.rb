8fc0e995bf016902821f394fd77cded72567aaa5
Lizzie <lchan217@gmail.com>
2019-05-27 15:39:13 -0500

    delete Restaurant city and state from seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-bens = Restaurant.create(name: "Bens", price_range:"$", parking: "Street", wifi: "No", occasion: "Casual", takeS_reservations:"No", city:"NYC" , state: "New York", rating: "5", comment: "good price for quantity and quality")
+bens = Restaurant.create(name: "Bens", price_range:"$", parking: "Street", wifi: "No", occasion: "Casual", takeS_reservations:"No", rating: "5", comment: "good price for quantity and quality")
 nyc= Location.create(state:"New York", city:"New York City")
 bens.locations << nyc
 grandma= Item.create(name: "Grandma Slice", vegetarian: "Yes", calories: "500", price: "$3.5", category:"Main Meal")
 pepper= Item.create(name: "Pepperoni", vegetarian: "No", calories: "750", price: "$4.5", category:"Main Meal")
 bens.items << [grandma, pepper]
 
-melt = Restaurant.create(name: "Melt", price_range:"$$", parking: "Street", wifi: "Yes", occasion: "Casual", takeS_reservations:"No", city:"Cleveland", state: "Ohio", rating: "5", comment: "one of my favorite places to go to when I visit!" )
+melt = Restaurant.create(name: "Melt", price_range:"$$", parking: "Street", wifi: "Yes", occasion: "Casual", takeS_reservations:"No", rating: "5", comment: "one of my favorite places to go to when I visit!" )
 cleveland= Location.create(state:"Ohio", city:"Cleveland")
 melt.locations << cleveland
 pickles= Item.create(name: "Fried Pickles", vegetarian: "Yes", calories: "500", price: "$7", category:"Appetizer")
 pepper= Item.create(name: "Wake and Bacon", vegetarian: "No", calories: "I don't want to know" , price: "$8", category:"Main Meal")
 melt.items << [pickles, pepper]
 
-cheesies = Restaurant.create(name: "Cheesies", price_range:"$$", parking: "Street", wifi: "Yes", occasion: "Full Meal", takeS_reservations:"No", city:"Chicago", state: "Illinois", rating: "5", comment: "chicago favorite" )
+cheesies = Restaurant.create(name: "Cheesies", price_range:"$$", parking: "Street", wifi: "Yes", occasion: "Full Meal", takeS_reservations:"No", rating: "5", comment: "chicago favorite" )
 chicago1= Location.create(state:"Illinois", city:"Chicago")
 cheesies.locations << chicago1
 mac= Item.create(name: "The Mac" , vegetarian: "Yes", calories: "I don't want to know" , price: "$8", category:"Main Meal")
@@ -21,14 +21,14 @@ caprese= Item.create(name: "Caprese", vegetarian: "Yes", calories: "I don't want
 cheesies.items << [mac, tomato, caprese]
 
 
-wasabi = Restaurant.create(name: "Wasabi", price_range:"$$", parking: "Street", wifi: "Yes", occasion: "Full Meal", takeS_reservations:"No", city:"Chicago", state: "Illinois", rating: "5", comment: "best ramen in the city")
+wasabi = Restaurant.create(name: "Wasabi", price_range:"$$", parking: "Street", wifi: "Yes", occasion: "Full Meal", takeS_reservations:"No", rating: "5", comment: "best ramen in the city")
 chicago2= Location.create(state:"Illinois", city:"Chicago")
 wasabi.locations << chicago2
 orig= Item.create(name: "Original Tonkotsu", vegetarian: "No", calories: "800", price: "$14", category:"Main Meal")
 edamame= Item.create(name: "Edamame", vegetarian: "Yes", calories: "300", price: "$4.5", category:"Appetizer")
 wasabi.items << [orig, edamame]
 
-mcds = Restaurant.create(name: "Mcds" , price_range:"$", parking: "Parking lot", wifi: "Yes", occasion: "Fast Food", takeS_reservations:"No", city:"Naperville", state: "Illinois", rating: "5", comment: "breakfast is the best" )
+mcds = Restaurant.create(name: "Mcds" , price_range:"$", parking: "Parking lot", wifi: "Yes", occasion: "Fast Food", takeS_reservations:"No", rating: "5", comment: "breakfast is the best" )
 naperville= Location.create(state:"Illinois", city:"Naperville")
 nyc2= Location.create(state:"New York", city:"New York City")
 cleveland3= Location.create(state:"Ohio", city:"Cleveland")
