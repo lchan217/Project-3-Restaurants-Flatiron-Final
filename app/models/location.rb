806d27fa3794cea3224b935d02a762be409766ef
Lizzie <lchan217@gmail.com>
2019-05-29 11:27:00 -0500

    final edit to location validation

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ActiveRecord::Base
   belongs_to :user, optional: true
   belongs_to :restaurant, optional: true
-  validates_presence_of :name, { message: "Name can't be blank"}
+  validates_presence_of :state, { message: "State can't be blank"}
   #validate :at_least_one
 
   def city_and_state
