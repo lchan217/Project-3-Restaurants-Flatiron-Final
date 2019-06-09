bf72ece7ef69a6e391edd964af4aa89d7c836fde
Lizzie <lchan217@gmail.com>
2019-06-09 09:29:17 -0500

    add minimum length to pw validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true
   validates :username, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   def self.from_omniauth(auth)
    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
