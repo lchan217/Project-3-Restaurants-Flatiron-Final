4e4a74a86f61d7fd5e986d5138a0673021869cf7
Lizzie <lchan217@gmail.com>
2019-06-09 09:27:18 -0500

    add rating validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,7 @@ class Restaurant < ApplicationRecord
   has_many :users, through: :locations
 
   validates_presence_of :name, { message: "Name can't be blank"}
+  validate :rating_not_higher_than_five
 
   accepts_nested_attributes_for :locations #,reject_if: proc { |attributes| attributes[:state].blank? }
 
@@ -30,5 +31,11 @@ class Restaurant < ApplicationRecord
 
   def self.options
     self.select(:state).distinct.order(:state)
+  end
+
+  def rating_not_higher_than_five
+    if rating.to_i > 5 || rating.to_i < 1
+     errors.add(:rating, "Rating must be between 1 and 5")
+    end
   end
 end
