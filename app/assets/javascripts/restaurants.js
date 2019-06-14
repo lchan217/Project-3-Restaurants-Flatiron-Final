e4f99e35e5551d59beae3e792e8feb52b873ca3d
Lizzie <lchan217@gmail.com>
2019-06-14 15:47:48 -0500

    add deleted code back in

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,108 +1,16 @@
 $(function () {
-  listenForAllItemsClick()
-	listenForItemClick()
-	listenForNewRestaurant()
+   $('form#new_restaurant').submit(function(event) {
+     event.preventDefault();
+     var values = $(this).serialize();
+     var posting = $.post('/restaurants', values);
+
+     posting.done(function(data){
+             const newRestaurant = new Restaurant({state: data.locations[0].state, city: data.locations[0].city, name: data.name, price_range: data.price_range, parking: data.parking, wifi: data.wifi, occasion: data.occasion, takeS_reservations: data.takeS_reservations, rating: data.rating, comment: data.comment})
+     				newRestaurant.newFormat()
+              $("#restResult").append($(newRestaurant.newFormat()));
+    });
+  });
 });
-
-// ================================================================
-
-function listenForAllItemsClick() {
-	$('button#items').on('click', function (event) {
-		$(this).prop('disabled', true);
-		event.preventDefault()
-		getItems(this.dataset.id)
-	})
-}
-
-function getItems(id){
-	const url = 'http://localhost:3000/restaurants/'+ id + '/items.json'
-  fetch(url)
-	.then(response => response.json())
-	.then(json => {json.map(element =>
-		document.getElementById('allTheItems-'+ id).innerHTML += `<div> ${element.name} </div>`
-		)
-
-	})
-}
-
-// ================================================================
-
-function listenForItemClick(){
-	$('a#show-item').on('click', function (event) {
-		event.preventDefault()
-		getItem(this.dataset.id)
-	})
-}
-
-function getItem(id){
-	document.getElementById('itemDetail-'+id).innerHTML = ''
-
-	const path = this.location.pathname
-	const url = 'http://localhost:3000' + path + '/items/' + id + '.json'
-
-	fetch(url)
-	.then(response => response.json())
-	.then(json =>
-	document.getElementById('itemDetail-'+id).innerHTML += `<div> Vegetarian: ${json.vegetarian} </div>
-		<div> Calories: ${json.calories} </div>
-		<div> Price: ${json.price} </div>
-		<div> Category: ${json.category} </div>`)
-}
-
-// ================================================================
-
-function listenForNewRestaurant(){
-	$('form#new_restaurant').on('submit', function (event) {
-		$(this).prop('disabled', true);
-		event.preventDefault()
-    createRestaurant()
-	})
-}
-
-function createRestaurant(){
-    let newObject = {name: document.querySelector('#restaurant_name').value,
-    price_range:  document.querySelector('#restaurant_price_range').value,
-    takeS_reservations:  document.querySelector('#restaurant_takeS_reservations').value,
-    parking:  document.querySelector('#restaurant_parking').value,
-    wifi:  document.querySelector('#restaurant_wifi').value,
-    occasion:  document.querySelector('#restaurant_occasion').value,
-    rating: document.querySelector('#restaurant_rating').value,
-    comment: document.querySelector('#restaurant_comment').value,
-
-    locations_attributes: {"0": {city: document.querySelector('#restaurant_locations_attributes_0_city').value,
-    state: document.querySelector('#restaurant_locations_attributes_0_state').value,
-    user_id: document.querySelector('#restaurant_locations_attributes_0_user_id').value
-  }
-    }
-  }
-   validateRestaurant(newObject)
-}
-
-function validateRestaurant(newObject){
-  if (newObject.name === "" || newObject.locations_attributes[0].state === "" || newObject.rating ===""){
-    alert("Please refresh page, then fill in name, rating, and state")
-  } else if (newObject.rating > 5 || newObject.rating < 1) {
-      alert("Please refresh page, then a rating between 1 and 5")
-  } else {
-    saveRestaurant(newObject)
-  }
-}
-
-function saveRestaurant(newObject){
-	fetch('http://localhost:3000/restaurants', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(newObject)
-	})
-	.then(response => response.json())
-	.then(json => console.log(json))
-  let newRestaurant = new Restaurant(newObject);
-  newRestaurant.newFormat()
-}
-
-// ================================================================
 
 class Restaurant {
   constructor(object) {
@@ -114,25 +22,20 @@ class Restaurant {
     this.takeS_reservations = object.takeS_reservations;
     this.rating = object.rating;
     this.comment = object.comment;
-    this.state = object.locations_attributes[0].state
-    this.city = object.locations_attributes[0].city
-    this.user_id = object.locations_attributes[0].user_id
+		this.city = object.city;
+		this.state = object.state;
   }
 }
 
 Restaurant.prototype.newFormat = function(){
-
-		document.querySelector('#formResults').innerHTML +=
-						`Results:
-
-            <div id="restName"> Name: ${this.name} </div>
-						<div id="restPrice"> Price Range: ${this.price_range} </div>
-						<div id="restWifi"> Wifi: ${this.wifi} </div>
-						<div id="restOccasion"> Occasion: ${this.occasion} </div>
-						<div id="restReserves"> Reservations: ${this.takeS_reservations} </div>
-						<div id="restRating"> Rating: ${this.rating} </div>
-						<div id="restComment"> Comment: ${this.comment} </div>
-						<div id="restCity"> City: ${this.city} </div>
-						<div id="restState"> State: ${this.state} </div>`
-
+	return `<div id="restName"> Name: ${this.name} </div>
+					<div id="restPrice"> Price Range: ${this.price_range} </div>
+					<div id="restWifi"> Wifi: ${this.wifi} </div>
+					<div id="restOccasion"> Occasion: ${this.occasion} </div>
+					<div id="restReserves"> Reservations: ${this.takeS_reservations} </div>
+					<div id="restRating"> Rating: ${this.rating} </div>
+					<div id="restComment"> Comment: ${this.comment} </div>
+					<div id="restCity"> City: ${this.city} </div>
+					<div id="restState"> State: ${this.state} </div>
+					`
 }
