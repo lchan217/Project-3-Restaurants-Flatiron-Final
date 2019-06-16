cf3db307239ae648a9bc81711e43f88cda586412
Lizzie <lchan217@gmail.com>
2019-06-15 19:19:55 -0500

    fix location search error

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,20 +1,24 @@
 class LocationsController < ApplicationController
     before_action :require_login
-  def all_restaurants_in_db
-    if params[:query_search] && params[:query_existing] == ""
-      @location = Location.search(params[:query_search])
-      @state = params[:query_search]
-    elsif params[:query_existing] && params[:query_search] == ""
-      @location = Location.search(params[:query_existing])
-      @state = params[:query_1existing]
-    else
-      @location = Location.search("all")
-      @state = "All results"
-    end
-    render 'all_restaurants_in_db'
-  end
+    def all_restaurants_in_db
+       if params[:query_search] && params[:query_existing] == ""
+         @location = Location.search(params[:query_search])
+         if @location.sort_by{|obj| obj.id} == Location.all.sort_by{|obj| obj.id}
+           @state = "No results found - here are All Restaurants"
+         else
+           @state = params[:query_search]
+         end
+       elsif params[:query_existing] && params[:query_search] == ""
+         @location = Location.search(params[:query_existing])
+         @state = params[:query_1existing]
+       else
+         @location = Location.search("all")
+         @state = "All results"
+       end
+       render 'all_restaurants_in_db'
+     end
 
-  def restaurant_params
-    params.require(:location).permit(:query_search, :query_existing, :city, :state)
-  end
-end
+     def restaurant_params
+       params.require(:location).permit(:query_search, :query_existing, :city, :state)
+     end
+   end
