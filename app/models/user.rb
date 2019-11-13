2bbb5c7e5fd28162c3db7ad9fed51565e3a39482
Lizzie <lchan217@gmail.com>
2019-11-13 16:08:19 -0700

    combine from_omniauth in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   def self.from_omniauth(auth)
-   where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+   where(provider: auth.provider, uid: auth.uid || email: auth.info.email).first_or_initialize.tap do |user|
      user.email = auth.info.email
+     user.name = auth.info.name
      user.uid = auth.uid
      user.provider = auth.provider
      user.avatar_url = auth.info.image
@@ -21,12 +22,4 @@ class User < ActiveRecord::Base
      user.save!
    end
  end
-
-  def self.from_omniauth(auth)
-    # Creates a new user only if it doesn't exist
-    where(email: auth.info.email).first_or_initialize do |user|
-      user.name = auth.info.name
-      user.email = auth.info.email
-    end
-  end
 end
