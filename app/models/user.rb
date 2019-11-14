23dc4ee1410f7f1a8c8b230f3ddbecda3b29cefc
Lizzie <lchan217@gmail.com>
2019-11-14 16:35:13 -0700

    comment out username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, presence: true
-  validates :username, uniqueness: true
+  # validates :username, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
   def self.from_github_omniauth(auth)
