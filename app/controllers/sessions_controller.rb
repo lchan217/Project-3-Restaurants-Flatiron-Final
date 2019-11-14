c535564735cc5b58928f52a9e69785359d23185c
Lizzie <lchan217@gmail.com>
2019-11-14 16:28:58 -0700

    add google omni auth back

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
   def github_create
     if auth_hash = request.env["omniauth.auth"]
-      @user = User.from_omniauth(auth_hash)
+      @user = User.from_github_omniauth(auth_hash)
       session[:user_id] = @user.id
       redirect_to restaurants_path
     else
@@ -17,21 +17,23 @@ class SessionsController < ApplicationController
     end
   end
 
-  # def googleAuth
-  #   # Get access tokens from the google server
-  #   access_token = request.env["omniauth.auth"]
-  #     user = User.from_omniauth(access_token)
-  #     log_in(user)
-  #     # Access_token is used to authenticate request made from the rails application to the google server
-  #     user.google_token = access_token.credentials.token
-  #     # Refresh_token to request new access_token
-  #     # Note: Refresh_token is only sent once during the first request
-  #     refresh_token = access_token.credentials.refresh_token
-  #     user.google_refresh_token = refresh_token if refresh_token.present?
-  #     user.save
-  #     redirect_to root_path
-  #   end
-  # end
+  def google_create
+      # Get access tokens from the google server
+      if access_token = request.env["omniauth.auth"]
+        user = User.from_google_omniauth(access_token)
+        log_in(user)
+        # Access_token is used to authenticate request made from the rails application to the google server
+        user.google_token = access_token.credentials.token
+        # Refresh_token to request new access_token
+        # Note: Refresh_token is only sent once during the first request
+        refresh_token = access_token.credentials.refresh_token
+        user.google_refresh_token = refresh_token if refresh_token.present?
+        user.save
+        redirect_to restaurants_path
+      else
+        redirect_to root_path
+      end
+  end
 
   def create
       @user = User.find_by(username: params[:user][:username])
