e77e740de29f6c36f0affaca6e8ea122c9bdd90d
Lizzie <lchan217@gmail.com>
2019-11-13 17:01:11 -0700

    restore

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,10 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def omni_create
+  def github_create
     if auth_hash = request.env["omniauth.auth"]
-      @user = User.from_github_omniauth(auth_hash)
+      @user = User.from_omniauth(auth_hash)
       session[:user_id] = @user.id
-      redirect_to restaurants_path
     else
       redirect_to root_path
     end
@@ -19,17 +18,20 @@ class SessionsController < ApplicationController
 
   def googleAuth
     # Get access tokens from the google server
-    access_token = request.env["omniauth.auth"]
-    user = User.from_google_omniauth(access_token)
-    log_in(user)
-    # Access_token is used to authenticate request made from the rails application to the google server
-    user.google_token = access_token.credentials.token
-    # Refresh_token to request new access_token
-    # Note: Refresh_token is only sent once during the first request
-    refresh_token = access_token.credentials.refresh_token
-    user.google_refresh_token = refresh_token if refresh_token.present?
-    user.save
-    redirect_to root_path
+    if access_token = request.env["omniauth.auth"]
+      user = User.from_omniauth(access_token)
+      log_in(user)
+      # Access_token is used to authenticate request made from the rails application to the google server
+      user.google_token = access_token.credentials.token
+      # Refresh_token to request new access_token
+      # Note: Refresh_token is only sent once during the first request
+      refresh_token = access_token.credentials.refresh_token
+      user.google_refresh_token = refresh_token if refresh_token.present?
+      user.save
+      redirect_to restaurants_path
+    else
+      redirect_to root_path
+    end
   end
 
   def create
