d5617e3a75c8a199dd547d7bb6fabdb571638648
Lizzie <lchan217@gmail.com>
2019-11-13 17:10:42 -0700

    remove omniauth google

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,11 +22,11 @@ class User < ActiveRecord::Base
    end
  end
 
- def self.from_omniauth(auth)
-  # Creates a new user only if it doesn't exist
-  where(email: auth.info.email).first_or_initialize do |user|
-    user.name = auth.info.name
-    user.email = auth.info.email
-  end
- end
+ # def self.from_omni(auth)
+ #  # Creates a new user only if it doesn't exist
+ #  where(email: auth.info.email).first_or_initialize do |user|
+ #    user.name = auth.info.name
+ #    user.email = auth.info.email
+ #  end
+ # end
 end
