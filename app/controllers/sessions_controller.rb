c69cc03f8223d389993093068cd564716de6f373
Lizzie <lchan217@gmail.com>
2019-11-14 18:32:26 -0700

    restore

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,10 @@ class SessionsController < ApplicationController
   def github_create
     if auth_hash = request.env["omniauth.auth"]
       @user = User.from_github_omniauth(auth_hash)
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to restaurants_path
-      else
-        flash[:notice] = "Account doesn't exist or is already registered. Please try again."
-        redirect_to root_path
-      end
+      session[:user_id] = @user.id
+      redirect_to restaurants_path
+    else
+      redirect_to root_path
     end
   end
 
@@ -24,19 +21,16 @@ class SessionsController < ApplicationController
       # Get access tokens from the google server
       if access_token = request.env["omniauth.auth"]
         user = User.from_google_omniauth(access_token)
-        if user.save
-          # Access_token is used to authenticate request made from the rails application to the google server
-          user.google_token = access_token.credentials.token
-          # Refresh_token to request new access_token
-          # Note: Refresh_token is only sent once during the first request
-          refresh_token = access_token.credentials.refresh_token
-          user.google_refresh_token = refresh_token if refresh_token.present?
-          user.save
-          redirect_to restaurants_path
-        else
-          flash[:notice] = "Account doesn't exist or is already registered. Please try again."
-          redirect_to root_path
-        end
+        # Access_token is used to authenticate request made from the rails application to the google server
+        user.google_token = access_token.credentials.token
+        # Refresh_token to request new access_token
+        # Note: Refresh_token is only sent once during the first request
+        refresh_token = access_token.credentials.refresh_token
+        user.google_refresh_token = refresh_token if refresh_token.present?
+        user.save
+        redirect_to restaurants_path
+      else
+        redirect_to root_path
       end
   end
 
